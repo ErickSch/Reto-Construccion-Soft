466fb41aeff4e5fcdfe239193f1778c75e14ad9c
Erick Schiller Echavarría <erickschiller@hotmail.com>
2023-06-03 19:58:33 -0600

    Delete Repo_api directory

--- Repo_api/index.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/", (req, res) => res.send("Hola Mundo!"));
-router.get("/ping", (req, res) => res.send("Pong!"));
-
-export default router;
